e3e5ca916fd8a9a8dfe05de53dc1a1466431398e
raj-aryan7 <rajaryan100raj@gmail.com>
2026-03-06 20:07:56 +0530

    Update preprocess_data.py

--- Dataset/preprocess_data.py
+++ Dataset/preprocess_data.py
@@ -1,1 +1,10 @@
+from tape import TAPETokenizer
 
+tokenizer = TAPETokenizer(vocab='iupac')
+
+sequence = "MKVLYNLV"
+
+tokens = tokenizer.encode(sequence)
+
+print("Original sequence:", sequence)
+print("Tokenized sequence:", tokens)
